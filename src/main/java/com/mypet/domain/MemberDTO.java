4335caacbdc60e21487d77e1442bf75d4d1e64a5
hololog <93326521+hololog@users.noreply.github.com>
2022-03-31 12:16:54 +0900

    fix memberDAO

--- src/main/java/com/mypet/domain/MemberDTO.java
+++ src/main/java/com/mypet/domain/MemberDTO.java
@@ -1,5 +1,21 @@
 package com.mypet.domain;
 
-public class MemberDTO {
+import java.sql.Timestamp;
 
+import lombok.Data;
+
+@Data
+public class MemberDTO {
+	private String user_id;
+	private String email;
+	private String nickname;
+	private String password;
+	private String name;
+	private String phone_no;
+	private String zipcode;
+	private String address;
+	private String detail_address;
+	private String member_point;
+	private Timestamp last_access;
+	private Timestamp join_date;
 }
--- src/main/java/com/mypet/service/MemberService.java
+++ src/main/java/com/mypet/service/MemberService.java
@@ -1,5 +1,5 @@
 package com.mypet.service;
 
-public class MemberService {
+public interface MemberService {
 
 }
