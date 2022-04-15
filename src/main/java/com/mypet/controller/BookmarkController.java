010eb64ee0660ced9b8339266ec38298a954bbf5
hololog <93326521+hololog@users.noreply.github.com>
2022-04-15 17:53:45 +0900

    Update BookmarkController.java

--- src/main/java/com/mypet/controller/BookmarkController.java
+++ src/main/java/com/mypet/controller/BookmarkController.java
@@ -1,16 +1,19 @@
 package com.mypet.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.mypet.domain.BoardDTO;
 import com.mypet.domain.BookmarkDTO;
 import com.mypet.service.BookmarkService;
 import com.mypet.service.FindboardService;
@@ -21,17 +24,6 @@ public class BookmarkController {
 	@Autowired
 	public BookmarkService bookmarkService;
 	
-//	@RequestMapping(value = "/findboard/bookmark")
-//	public BookmarkDTO bookmark(@RequestParam int findboardNum, HttpSession session) {
-//		BookmarkDTO bookmarkDTO = new BookmarkDTO();
-//		
-//		bookmarkDTO.setFindboardNum(findboardNum);
-//		bookmarkDTO.setEmail((String)session.getAttribute("email"));
-//		
-//		BookmarkService.updateBookmark(bookmarkDTO);
-//		
-//		return ;
-//	}
 	@Autowired
 	public FindboardService findboardService;
 	
@@ -69,31 +61,27 @@ public class BookmarkController {
 		return bookmarkService.getBookmarkNum(findboardNum);
 	}
 	
-//	private Map<String, Object> getTargetUserAndBoard() {
-//        boolean findTarget = false;
-//        long boardTotal = boardRepository.count();
-//        int findSize = 30;
-//
-//        Map<String, Object> target = new HashMap<>();
-//
-//        List<UserEntity> userEntities = userRepository.findAll();
-//
-//        for (UserEntity user : userEntities) {
-//            for (int index = 0; index <= boardTotal / findSize; index += findSize) {
-//                Page<BoardEntity> boardEntityPage = boardRepository.findAll(PageRequest.of(index, findSize));
-//                List<BoardEntity> boardEntities = boardEntityPage.getContent().stream().filter(entity -> entity.getWriter().equals(user) == false).collect(Collectors.toList());
-//                for (BoardEntity board : boardEntities) {
-//                    if (goodContentsHistoryRepository.findByUserAndBoard(user, board).isPresent() == false) {
-//                        target.put("targetUser", user);
-//                        target.put("targetBoard", board);
-//                        findTarget = true;
-//                        break;
-//                    }
-//                }
-//                if (findTarget) { break; }
-//            }
-//            if (findTarget) { break; }
-//        }
-//        return target;
-//    }
+//	@ResponseBody
+//	@RequestMapping(value="/wordSearchShow", method=RequestMethod.GET, produces="text/plain;charset=UTF-8")
+//	public String wordSearchShow(HttpServletRequest request) {
+//				
+//		String searchType = request.getParameter("searchType");
+//		String searchWord = request.getParameter("searchWord");
+//		
+//		Map<String, String> paraMap = new HashMap<>();
+//		paraMap.put("searchType", searchType);
+//		paraMap.put("searchWord", searchWord);
+//		
+//		List<String> wordList = findboardService.wordSearchSHow(paraMap);
+//		
+//		JSONArray jsonArr = new JSONArray(); 
+//			if(wordList != null) {
+//				for(String word : wordList) {
+//					JSONObject jsonObj = new JSONObject();
+//					jsonObj.put("word", word);			
+//					jsonArr.put(jsonObj);
+//				}
+//			}
+//		return jsonArr.toString();
+//	}
 } 
